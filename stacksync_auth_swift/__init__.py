38e070fa59fbc631500ab849db773479c264f47e
Adrian Moreno <adrian.moreno@urv.cat>
2014-07-04 10:32:51 +0200

    updated to 1.0.40

--- stacksync_auth_swift/__init__.py
+++ stacksync_auth_swift/__init__.py
@@ -3,6 +3,6 @@ StackSync Authentication middleware for Swift
 """
 
 #: Version information ``(major, minor, revision)``.
-__version_info__ = (1, 0, 39)
+__version_info__ = (1, 0, 40)
 #: Version string ``'major.minor.revision'``.
 __version__ = '.'.join(map(str, __version_info__))
